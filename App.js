58232256e1b7d7ff4e1e7897ffe474e926268759
davidliberal0 <davidliberal0@gmail.com>
2022-11-03 10:38:45 -0400

    create component for rendering goal-items

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import {
   ScrollView,
   FlatList,
 } from "react-native";
+import GoalItem from "./components/GoalItem";
 import { ReloadInstructions } from "react-native/Libraries/NewAppScreen";
 
 export default function App() {
@@ -39,11 +40,7 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem text={itemData.item.text} />;
           }}
         />
       </View>
@@ -76,14 +73,5 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 4,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-  },
-  goalText: {
-    color: "white",
   },
 });
