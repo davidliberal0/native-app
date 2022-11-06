5f470b72ee37f77a6744a8284a00caa36cf97848
davidliberal0 <davidliberal0@gmail.com>
2022-11-06 01:51:19 -0400

    add styling for ios delete goal press

--- components/GoalItem.jsx
+++ components/GoalItem.jsx
@@ -3,7 +3,11 @@ import { View, Text, StyleSheet, Pressable } from "react-native";
 
 const GoalItem = (props) => {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
+    <Pressable
+      android_ripple={{ color: "red" }}
+      onPress={props.onDeleteItem.bind(this, props.id)}
+      style={({ pressed }) => (pressed ? styles.pressedItem : null)}
+    >
       <View style={styles.goalItem}>
         <Text style={styles.goalText}>{props.text}</Text>
       </View>
@@ -17,6 +21,9 @@ const styles = StyleSheet.create({
     padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
   goalText: {
     color: "white",
